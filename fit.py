e61d2a29a43d9c86ccdb6ef282012aafe93ad352
Tyler <tjb269@cornell.edu>
2022-09-06 23:45:45 -0400

    added custom numerical function definitions to Fit expressions

--- fit.py
+++ fit.py
@@ -1,4 +1,5 @@
 import sympy as sp
+from sympy.utilities.lambdify import implemented_function
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
@@ -11,13 +12,19 @@ import time
 
 class Fit:
 
-  def __init__(self, data, expr, arg = "x"):
+  def __init__(self, data, expr, arg = "x", definitions = None):
 
     # The Data object to fit, which wraps the numerical data as data.x, data.y, and data.cov.
     self.data = data
 
+    # Attach custom numerical definitions (lambda expressions) to custom functions named in the 'expr' string.
+    if definitions is not None:
+      custom_functions = {name: implemented_function(name, impl) for name, impl in definitions.items()}
+    else:
+      custom_functions = None
+
     # Parse the model function and independent variable strings into SymPy expressions.
-    self.sp_expr = sp.parse_expr(expr)
+    self.sp_expr = sp.parse_expr(expr, local_dict = custom_functions)
     self.sp_arg = sp.Symbol(arg)
 
     # Identify the fit parameters as the set of all unbound symbols, minus the independent variable.
@@ -35,6 +42,7 @@ class Fit:
 
     # Convert the SymPy model function, jacobian, and hessian into NumPy functions.
     self.np_expr = sp.lambdify([self.sp_arg, *self.sp_params], self.sp_expr)
+    # TODO: can define as sympy.Matrix object, then lambdify automatically returns desired NumPy shape!!
     self.np_jac = [sp.lambdify([self.sp_arg, *self.sp_params], df_dp) for df_dp in self.sp_jac]
     self.np_hess = [[sp.lambdify([self.sp_arg, *self.sp_params], df_dpdq) for df_dpdq in row] for row in self.sp_hess]
 
@@ -45,24 +53,24 @@ class Fit:
     self.guess = guess if guess is not None else np.ones(len(self.sp_params))
 
     # Minimize the chi-squared using BFGS, repeated with random steps in the initial conditions ("basin hopping").
-    # self.opt_result = opt.basinhopping(
-    #   self.chi2,
-    #   x0 = self.guess,
-    #   minimizer_kwargs = {
-    #     "method": "BFGS",
-    #     "jac": self.chi2_jac
-    #   },
-    #   disp = True
-    # ).lowest_optimization_result
+    self.opt_result = opt.basinhopping(
+      self.chi2,
+      x0 = self.guess,
+      minimizer_kwargs = {
+        "method": "BFGS",
+        "jac": self.chi2_jac
+      },
+      disp = True
+    ).lowest_optimization_result
 
     # how to use basinhopping intelligently to keep things fast? single local minimize is ~100x faster...
     # maybe try single minimization at given seed first, and proceed to basin hopping if result is poor? e.g. didn't converge, or chi2 too big?
-    self.opt_result = opt.minimize(
-      self.chi2,
-      x0 = self.guess,
-      method = "BFGS",
-      jac = self.chi2_jac
-    )
+    # self.opt_result = opt.minimize(
+    #   self.chi2,
+    #   x0 = self.guess,
+    #   method = "BFGS",
+    #   jac = self.chi2_jac
+    # )
     
     self.p_opt = self.opt_result.x
     self.p_cov = np.linalg.inv(self.chi2_hess(self.p_opt)) # TODO: should there be a 1/2 here or not????
@@ -117,7 +125,7 @@ if __name__ == "__main__":
   err = np.ones(len(x)) * std
   data = Data(x, y, err = err)
 
-  fit = Fit(data, "a * cos(b*x) + c * x**2")
+  fit = Fit(data, "a * cos(b*x) + c * f(x)", definitions = {"f": lambda t: x**2})
   fit.fit()
   print(fit.p_opt)
   print(np.sqrt(np.diag(fit.p_cov)))
